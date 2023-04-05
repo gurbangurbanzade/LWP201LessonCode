9bb28eb18a2d565a0bcff546e44e1b0a5e6a7c66
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2023-04-05 17:26:56 +0400

    js added

---
+++ 10-JsIntro/app.js
@@ -0,0 +1,152 @@
+// console.log("hello Lankaran");
+// console.log("hello Dunya");
+// salam;
+// alert(salam);
+// console.log(5);
+// console.log("mandalin 2 AZN");
+// console.log("mandalin 3 AZN");
+
+// let x = 3;
+// console.log("x deyisenin ilk deyeri: ", x);
+// x = 5;
+// console.log("x deyisenin ikinci deyeri: ", x);
+
+// let eded1 = 8;
+// let eded2 = 4;
+
+// let netice;
+
+// let neticeCem;
+
+// neticeCem = eded1 + eded2;
+// console.log("netice: ", neticeCem);
+
+// console.log("iki ededin cemi =", eded1 + eded2);
+// console.log("iki ededin ferqi =", eded1 - eded2);
+// console.log("iki ededin hasili =", eded1 * eded2);
+// console.log("iki ededin nisbeti =", eded1 / eded2);
+
+// let a = 3;
+// let soz = "salam";
+// let soz2 = "lankaran";
+
+// console.log(typeof a);
+// console.log(typeof soz);
+// console.log("cem:", a + soz);
+// console.log("sozlerin cemi:", soz + soz2);
+
+// let user;
+// user = prompt("Zehmet olmasa adinizi daxil edin");
+
+// console.log("salam" + " " + user);
+// let eded1;
+// let eded2;
+
+// eded1 = prompt("1-ci ededi daxil edin");
+// console.log("1-ci ededin tipi nedir?", typeof eded1);
+// eded1 = Number(prompt("1-ci ededi daxil edin"));
+// eded2 = Number(prompt("2-ci ededi daxil edin"));
+
+// let cem;
+
+// cem = eded1 + eded2;
+
+// console.log("iki ededin cemi =", cem);
+
+// let a = 8;
+// let b = 3;
+// let result = a > b; //true
+// result = a < b; // false
+// result = a >= b; //true
+// result = a <= b; // false
+
+// console.log(`${a} ${b} den boyukdurmu`, result);
+
+// let a = 1;
+// let b = 3;
+
+// if (a > b) {
+//   console.log(`${a} ededi ${b} ededinden boyukdur`);
+// } else if (a < b) {
+//   console.log(`${b} ededi ${a} ededinden boyukdur`);
+// } else {
+//   console.log(`${b} ededi ${a} ededine beraberdir`);
+// }
+
+// sabit-deyer-budur // kabab case
+// const sabitDeyerBudur = 35; // camel case
+// let sabit_deyer_budur = 35; // snake case
+
+// const sabitDeyer = 40;
+
+// let userDaxilEdenDeyer = prompt("ededi daxil edin");
+
+// if (sabitDeyer > userDaxilEdenDeyer) {
+// } else if (sabitDeyer < userDaxilEdenDeyer) {
+// } else {
+// }
+
+// let a = 4;
+// let b = 1;
+// let c = 5;
+
+// if (a > b) {
+//   console.log("1");
+// } else if (a >= b) {
+//   console.log("2");
+// } else if (b < a) {
+//   console.log("3");
+// } else if (b <= a) {
+//   console.log("4");
+// } else {
+//   console.log("5");
+// }
+
+// let a = 8;
+// let b = 11;
+// let c = 111;
+
+// if (a > b && a > c) {
+//   console.log("a en boyukdur");
+// } else if (b > a && b > c) {
+//   console.log("b boyukdur");
+// } else {
+//   console.log("c boyukdur");
+// }
+
+// let x = 2;
+// let y = 4;
+
+// if (x > y || x == y) {
+//   console.log("ya boyukdur ya beraberdir");
+// } else {
+//   console.log("y kicikdir");
+// }
+
+// let a = 4;
+// let b = 2;
+// // a += 5;
+
+// console.log("a=", a % b);
+
+console.log("Cem=");
+console.log("Hasil=");
+console.log("Ferq=");
+console.log("Nisbet=");
+console.log("Qaliq=");
+
+console.log("2 ededden hansi boyukdur");
+
+console.log("eded tekdir ya cutdur");
+let a = 5;
+let b = 3;
+
+// a > b ? console.log("a boyukdur") : console.log("b boyukdur");
+
+username = "lankaran";
+password = 12345;
+
+// istifadeci tapilmadi
+// parol yalnisdir
+// istifadeci adi yalnisdir
+// xos gelmissiniz
