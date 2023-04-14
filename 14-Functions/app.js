f4ed3f165753c2acbafb054ddb94b856e1ab14b0
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2023-04-14 11:41:19 +0400

    safas

---
+++ 14-Functions/app.js
@@ -0,0 +1,128 @@
+// let a = 3;
+// let b = 4;
+
+// let cem = a + b;
+
+// console.log("cem=", cem);
+
+// let a1 = 5;
+// let b1 = 6;
+
+// cem = a1 + b1;
+
+// console.log("cem=", cem);
+
+// function ceminiTap() {
+//   console.log("cem=");
+// }
+
+// ceminiTap();
+// ceminiTap();
+// ceminiTap();
+
+// function salamla() {
+//   //   console.log("salam");
+// }
+
+// salamla();
+// salamla();
+
+// function cemiTap(x, y) {
+//   console.log(x + y);
+// }
+
+// cemiTap(3, 5);
+// cemiTap(10, 11);
+
+// salamla("Hesen");
+
+// function salamla(ad) {
+//   console.log("salam " + ad);
+// }
+// salamla("Mursel");
+
+// let a = 5;
+// let b = 3;
+
+// function cem(x, y) {
+//   let cem = x + y;
+//   //   console.log(cem);
+//   return cem;
+//   console.log(cem);
+// }
+
+// // cem(a, b);
+
+// // console.log(cem(a, b));
+
+// function cemSQ(z) {
+//   let sq = z * z;
+//   console.log("sq", sq);
+// }
+
+// cemSQ(cem(a, b));
+
+// function modul(eded) {
+//   if (eded > 0) {
+//     console.log("musbet ededdir");
+//     // return "musbet ededdir";
+//   } else if (eded < 0) {
+//     console.log("menfi ededdir");
+//     // return "menfi ededdir";
+//   } else {
+//     console.log("eded 0");
+//     // return "eded 0";
+//   }
+// }
+// modul(-5);
+
+// let text = modul(-5);
+
+// console.log(text);
+
+// let soyad = "Qasimzade";
+// let yas = 25;
+// function salamla(ad) {
+//   let yas = 23;
+//   console.log("salam " + ad);
+//   return "salam " + ad;
+// }
+
+// console.log(salamla("cahangir").yas);
+// let ad = salamla("Necef");
+
+// function soyadElaveEt(salamlananAd, soyad) {
+//   console.log(salamlananAd + " " + soyad);
+// }
+
+// // soyadElaveEt(salamla("Necef"), "Qasimzade");
+// soyadElaveEt(ad, "Qasimzade");
+
+// function salamla(ad) {
+//   console.log("salam " + ad);
+// }
+// salamla("Mursel");
+
+// let salamlaArrow = (ad) => console.log("salam " + ad);
+
+// salamlaArrow("Kerim");
+
+// let sagollas = function (ad) {
+//   console.log("salgol " + ad);
+// };
+// sagollas("Cahangir");
+// function cemiTap(x, y) {
+//   console.log(x + y);
+// }
+
+// function cemiTap(x = 0, y = 0) {
+//   console.log(x + y);
+// }
+
+// cemiTap(3, 5);
+// let arr = [1, 2, 34];
+// let uzunluq=arr.length
+// let max = 0;
+// for (let i = 0; i < uzunluq; i++) {
+//   max = max + arr[i];
+// }
