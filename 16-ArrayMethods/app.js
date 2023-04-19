e42679d316ecae611c68334ed2e18a69d029840f
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2023-04-19 15:41:36 +0400

    dom added

---
+++ 16-ArrayMethods/app.js
@@ -0,0 +1,106 @@
+// result = arr[3];
+// result = arr.length;
+// let newArray = ["limon", "mandalin"];
+// result = arr.push("Celilabad");
+// result = arr.push(newArray);\
+// result = arr.pop();
+// result = arr.unshift("celilabad");
+// arr[0] = "celilabad";
+// result = arr.shift();
+// arr.shift();
+// result = arr.concat(newArray);
+
+// result = arr.includes("baki");
+// result = arr.indexOf("masalli");
+// result = arr.lastIndexOf("masalli");
+// result = arr.indexOf("masalli");
+
+// result = arr.reverse();
+
+// result = arr.slice(1, 5);
+
+// result = arr.splice(0, 2, "beylagan", "beylagan", "beylagan");
+
+// result = arr.toString();
+
+// function indexOfLankaran(soz) {
+//   let netice = false;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == soz) {
+//       netice = true;
+//       break;
+//     }
+//   }
+//   return netice;
+// }
+
+// console.log(
+//   "bizim yazdigimiz",
+
+//   indexOfLankaran("masalli")
+// );
+
+// console.log(
+//   "bizim yazdigimiz",
+
+//   indexOfLankaran("baki")
+// );
+
+let arr = [
+  "lankaran",
+  "baki",
+  "masalli",
+  "astara",
+  "lerik",
+  "masalli",
+  "lankaran",
+];
+let result;
+console.log("array ", arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// result = arr.forEach((elem, i) => {
+//   return elem, "indeks " + i;
+// });
+
+// result = arr.map((elem, i) => {
+//   return elem + "AZE";
+// });
+
+// function name(elem,i) {
+//   console.log(params);
+// }
+
+// let name = (params) => {
+//   console.log(params);
+// };
+
+// let arr2 = [3, 5, 6, 1, 0, -1];
+
+// result = arr2.filter((elem) => {
+//   return elem > 3;
+// });
+
+// result = arr.filter((elem) => {
+//   return elem == "lankaran";
+// });
+
+// result = arr2.every((elem) => {
+//   return elem > 0;
+// });
+
+// result = arr2.some((elem) => {
+//   return elem < 0;
+// });
+
+// let arr3 = [150, 50, 50];
+
+// result = arr3.reduce((sum, elem) => {
+//   return sum + elem;
+// });
+// console.log("result", result);
+
+let arr = ["lankaran", "baki", "masalli", "astara", "lerik", "masalli"];
