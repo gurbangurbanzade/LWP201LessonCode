49ca1e5b8745d57a0450a4364df36750de2f5765
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2023-04-27 14:12:25 +0400

    added new file

--- 19-InputEvenetLocaleStorage/LWP101/app.js
+++ 19-InputEvenetLocaleStorage/LWP101/app.js
@@ -1,97 +1,105 @@
-const inputCst = document.querySelector(".inputCst");
+// const inputCst = document.querySelector(".inputCst");
 
-const inputSurname = document.querySelector(".inputSurname");
-// inputCst.addEventListener("keypress", function (e) {
-//   console.log("eventimiz-" + e.type);
-//   //   console.log(e.target);
-// });
+// const inputSurname = document.querySelector(".inputSurname");
+// // inputCst.addEventListener("keypress", function (e) {
+// //   console.log("eventimiz-" + e.type);
+// //   //   console.log(e.target);
+// // });
 
-// inputCst.addEventListener("keyup", function (e) {
-//   console.log("eventimiz-" + e.type);
-//   //   console.log(e.target);
-// });
+// // inputCst.addEventListener("keyup", function (e) {
+// //   console.log("eventimiz-" + e.type);
+// //   //   console.log(e.target);
+// // });
 
-// inputCst.addEventListener("cut", function (e) {
-//   console.log("eventimiz-" + e.type);
-//   //   console.log(e.target);
-// });
+// // inputCst.addEventListener("cut", function (e) {
+// //   console.log("eventimiz-" + e.type);
+// //   //   console.log(e.target);
+// // });
 
-// inputCst.addEventListener("paste", function (e) {
-//   console.log("eventimiz-" + e.type);
-//   //   console.log(e.target);
-// });
+// // inputCst.addEventListener("paste", function (e) {
+// //   console.log("eventimiz-" + e.type);
+// //   //   console.log(e.target);
+// // });
 
-// inputCst.addEventListener("focus", function (e) {
-//   console.log("eventimiz-" + e.type);
-//   //   console.log(e.target);
-// });
+// // inputCst.addEventListener("focus", function (e) {
+// //   console.log("eventimiz-" + e.type);
+// //   //   console.log(e.target);
+// // });
 
-// inputCst.addEventListener("blur", function (e) {
-//   console.log("eventimiz-" + e.type);
-//   //   console.log(e.target);
-// });
+// // inputCst.addEventListener("blur", function (e) {
+// //   console.log("eventimiz-" + e.type);
+// //   //   console.log(e.target);
+// // });
 
-// inputCst.addEventListener("keydown", function (e) {
-//   console.log("eventimiz-" + e.type);
-//   //   console.log(e.target);
-// });
+// // inputCst.addEventListener("keydown", function (e) {
+// //   console.log("eventimiz-" + e.type);
+// //   //   console.log(e.target);
+// // });
 
-// inputCst.addEventListener("input", function (e) {
-//   console.log("eventimiz-" + e.type);
-//   //   console.log(e.target);
-// });
+// // inputCst.addEventListener("input", function (e) {
+// //   console.log("eventimiz-" + e.type);
+// //   //   console.log(e.target);
+// // });
 
-// function getEvent(e) {
-//   console.log("eventimiz-" + e);
-//   console.log(e.target);
-//   //   console.log("hello");
+// // function getEvent(e) {
+// //   console.log("eventimiz-" + e);
+// //   console.log(e.target);
+// //   //   console.log("hello");
+// // }
+
+// // getEvent();
+
+// // let x=5
+
+// const formCst = document.querySelector(".formCst");
+
+// const addBtn = document.querySelector(".addBtn");
+
+// console.log(JSON.parse(localStorage.getItem("arr")));
+// let arr = [];
+
+// if (JSON.parse(localStorage.getItem("arr")) !== null) {
+//   arr = JSON.parse(localStorage.getItem("arr"));
 // }
 
-// getEvent();
+// // let arr = [];
+// formCst.addEventListener("submit", function (e) {
+//   let obj = {
+//     name: "",
+//     surname: "",
+//     age: 5,
+//   };
 
-// let x=5
+//   obj.name = inputCst.value;
+//   obj.surname = inputSurname.value;
+//   console.log(obj);
 
-const formCst = document.querySelector(".formCst");
+//   arr.push(obj);
+//   console.log(arr);
 
-const addBtn = document.querySelector(".addBtn");
-let arr = [];
-formCst.addEventListener("submit", function (e) {
-  let obj = {
-    name: "",
-    surname: "",
-    age: 5,
-  };
+//   localStorage.setItem("arr", JSON.stringify(arr));
 
-  obj.name = inputCst.value;
-  obj.surname = inputSurname.value;
-  console.log(obj);
+//   console.log(JSON.parse(localStorage.getItem("arr")));
 
-  arr.push(obj);
-  console.log(arr);
+//   let testData = JSON.parse(localStorage.getItem("arr"));
+//   console.log(typeof testData[0].age);
 
-  localStorage.setItem("arr", JSON.stringify(arr));
+//   e.preventDefault();
+// });
 
-  console.log(JSON.parse(localStorage.getItem("arr")));
+// // console.log(inputCst.value);
 
-  let testData = JSON.parse(localStorage.getItem("arr"));
-  console.log(typeof testData[0].age);
+// // localStorage.setItem("name", "qurban");
+// // localStorage.setItem("surname", "qurbanzada");
+// // localStorage.setItem("name", "kenan");
 
-  e.preventDefault();
-});
+// // console.log(localStorage.getItem("name"));
+// // console.log(localStorage.getItem("surname"));
 
-// console.log(inputCst.value);
+// // sessionStorage.setItem("name", "qurban");
+// // sessionStorage.setItem("surname", "qurbanzada");
 
-// localStorage.setItem("name", "qurban");
-// localStorage.setItem("surname", "qurbanzada");
-// localStorage.setItem("name", "kenan");
+// // console.log(sessionStorage.getItem("name"));
+// // console.log(sessionStorage.getItem("surname"));
 
-// console.log(localStorage.getItem("name"));
-// console.log(localStorage.getItem("surname"));
-
-// sessionStorage.setItem("name", "qurban");
-// sessionStorage.setItem("surname", "qurbanzada");
-
-// console.log(sessionStorage.getItem("name"));
-// console.log(sessionStorage.getItem("surname"));
-
-// localStorage.clear();
+// // localStorage.clear();
---
+++ 19-InputEvenetLocaleStorage/LWP201/app.js
@@ -0,0 +1,88 @@
+const cstInput = document.querySelector(".cstInput");
+const surname = document.querySelector(".surname");
+const addBtn = document.querySelector(".addBtn");
+const users = document.querySelector(".users");
+const cstForm = document.querySelector(".cstForm");
+// cstInput.addEventListener("keydown", function (e) {
+//   console.log("eventimiz", e.type);
+// });
+// cstInput.addEventListener("keyup", function (e) {
+//   console.log("eventimiz", e.type);
+// });
+
+// cstInput.addEventListener("focus", function (e) {
+//   console.log("eventimiz", e.type);
+// });
+
+// cstInput.addEventListener("blur", function (e) {
+//   console.log("eventimiz", e.type);
+// });
+
+// cstInput.addEventListener("paste", function (e) {
+//   console.log("eventimiz", e.type);
+// });
+
+// cstInput.addEventListener("cut", function (e) {
+//   console.log("eventimiz", e.type);
+// });
+
+// cstInput.addEventListener("keydown", function (e) {
+//   console.log("eventimiz", e.type);
+// });
+
+// cstInput.addEventListener("input", function (e) {
+//   console.log("eventimiz", e.type);
+// });
+let arr = [];
+
+cstForm.addEventListener("submit", function (e) {
+  let obj = {
+    name: "",
+    surname: "",
+  };
+  console.log("name", cstInput.value);
+  console.log("surname", surname.value);
+
+  obj.name = cstInput.value;
+  obj.surname = surname.value;
+  arr.push(obj);
+  localStorage.setItem("dataBase", JSON.stringify(arr));
+  e.preventDefault();
+});
+// localStorage.setItem("name", "qurban");
+// localStorage.setItem("surname", "qurbanzada");
+// localStorage.setItem("name2", "namiq");
+
+// const obj = {
+//   name: "",
+//   surname: "",
+// };
+// obj.name = "qurban";
+// obj.surname = "qurbanzade";
+// console.log(obj);
+// arr.push(obj);
+// console.log("arr", arr);
+
+// // obj.name = "namiq";
+// // obj.surname = "abasov";
+// console.log(obj);
+// arr.push(obj);
+// console.log("arr", arr);
+// localStorage.setItem("arr", JSON.stringify(arr));
+// console.log(JSON.parse(localStorage.getItem("arr")));
+// // console.log(localStorage.getItem("name2"));
+
+// localStorage.clear();
+
+// // console.log("---------------------");
+// // let a = 5;
+// // console.log(a);
+// // a = 8;
+// // console.log(a);
+// // obj.name = "namiq";
+// // console.log(obj.name);
+// // obj.name = "qurban";
+// // console.log(obj.name);
+users.addEventListener("click", function () {
+  console.log(JSON.parse(localStorage.getItem("dataBase")));
+});
